4b34b436c59d9b4f6a533454415f7a1cf97c3822
CraciDenisStefan <crdenis13@gmail.com>
2023-06-20 16:44:05 +0200

    [FEAT]Creato Header e quasi finito

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,21 +1,107 @@
 <script>
 export default {
+    data(){
+        return{
+            menu:[
+                {
+                    label:'Characters',
+                    link:'#',
+                    active: false,
+                },
+                {
+                    label:'Comics',
+                    link:'#',
+                    active: false,
+                },
+                {
+                    label:'Movies',
+                    link:'#',
+                    active: false,
+                },
+                {
+                    label:'Tv',
+                    link:'#',
+                    active: false,
+                },
+                {
+                    label:'Games',
+                    link:'#',
+                    active: false,
+                },
+                {
+                    label:'Collectibles',
+                    link:'#',
+                    active: false,
+                },
+                {
+                    label:'Videos',
+                    link:'#',
+                    active: false,
+                },
+                {
+                    label:'Fans',
+                    link:'#',
+                    active: false,
+                },
+                {
+                    label:'News',
+                    link:'#',
+                    active: false,
+                },
+                {
+                    label:'Shop',
+                    link:'#',
+                    active: false,
+                },
+            ]
+        }
+    }
     
 }
 </script>
 <template>
    <header class="container d_flex">
-    <div class="logo"></div>
-    <div class="menu"></div>
+    <div class="logo">
+        <img src="../assets/img/dc-logo.png" alt="">
+    </div>
+    <div class="menu">
+        <ul class="d_flex">
+            <li v-for="(lista, index) in menu" :key="index">
+               <a :href="lista.link"> {{ lista.label }}</a>
+            </li>
+        </ul>
+    </div>
    </header>
 </template>
 <style scoped lang="scss">
     header{
         justify-content: space-between;
+        align-items: center;
     }
-    .logo, .menu{
-        width: 50px;
-        height: 50px;
-        background-color: red;
+    img{
+        width: 4rem;
+    }
+    ul{
+        list-style: none;
+        vertical-align: bottom;
+        li{
+            padding: 40px 0;
+            margin: 0 0.5rem;
+            font-size: 0.8rem;
+            border-bottom:5px solid white ;
+            
+            a{
+                text-decoration: none;
+                font-weight: 700;
+                text-transform: uppercase;
+                color:rgb(70, 70, 70)
+
+            }
+            &:hover{
+                border-bottom: 5px solid rgb(42, 86, 228);
+                color: rgb(42, 86, 228);
+               
+            }
+        }
     }
 </style>
